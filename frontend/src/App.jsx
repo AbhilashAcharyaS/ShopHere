728e24a1474fe82dd73835ddcfb4c5e2fb94d3aa
AbhilashAcharyaS <abhilashacharya599@gmail.com>
2026-01-28 13:37:40 +0530

    Setup routing and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,28 @@
+import { Routes,Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Login from "./pages/Login"
+import Cart from "./pages/Cart"
+import Collection from "./pages/Collection"
+import Orders from "./pages/Orders"
+import Product from "./pages/Product"
+import PlaceOrder from "./pages/PlaceOrder"
+
 function App() {
   return (
-    <div>
-      <h1 className="bg-red-400 text-9xl">Hello</h1>
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/contact" element={<Contact/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/cart" element={<Cart/>}></Route>
+        <Route path="/collection" element={<Collection/>}></Route>
+        <Route path="/Orders" element={<Orders/>}></Route>
+        <Route path="/product/:productId" element={<Product/>}></Route>
+        <Route path="/place-order" element={<PlaceOrder/>}></Route>
+      </Routes>
     </div>
   )
 }
