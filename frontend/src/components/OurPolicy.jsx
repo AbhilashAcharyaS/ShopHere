88f15d5a3c939a79e81096fbb60e73e33ba1b2fc
AbhilashAcharyaS <abhilashacharya599@gmail.com>
2026-02-02 22:11:49 +0530

    Design Newsletter

--- frontend/src/components/OurPolicy.jsx
+++ frontend/src/components/OurPolicy.jsx
@@ -5,7 +5,7 @@ const OurPolicy = () => {
   return (
     <div className="my-10">
       <div className="text-center text-3xl py-8">
-        <Title text1={"Our"} text2={"Policies"} />
+        <Title text1={"OUR"} text2={"POLICIES"} />
       </div>
       <div className="flex flex-col sm:flex-row justify-around gap-12 sm:gap-2 text-center py-10 text-xs sm:text-sm md:text-base text-gray-700 ">
         <div>
