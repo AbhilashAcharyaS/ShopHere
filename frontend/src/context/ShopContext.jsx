0e2ac59f10f1d80684348f0d989f625b69c5627f
AbhilashAcharyaS <abhilashacharya599@gmail.com>
2026-03-31 14:23:20 +0530

    Feat: Cart page

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
+import {useNavigate} from "react-router-dom"
 
 export const ShopContext=createContext();
 
@@ -7,12 +8,13 @@ const ShopContextProvider=(props)=>{
     const [search,setSearch]=useState("");
     const [showSearch,setShowSearch]=useState(false);
     const [cartItems, setCartItems]=useState({});
+    const navigate = useNavigate()
 
     const addToCart=async(itemId,size)=>{
-      if(!size){
-        toast.error("Select Product Size!");
-        return;
-      }
+      // if(!size){
+      //   toast.error("Select Product Size!");
+      //   return;
+      // }
       let cartData= structuredClone(cartItems);
       if(cartData[itemId]){
         if(cartData[itemId][size]){
@@ -44,6 +46,30 @@ const ShopContextProvider=(props)=>{
         }
       }
       return totalCount;
+    }
+
+    const updateQuantity=async(itemId, size, quantity)=>{
+      let cartData=structuredClone(cartItems);
+      cartData[itemId][size]=quantity;
+      setCartItems(cartData)
+    }
+
+    const getCartAmount=()=>{
+      let totalAmount=0;
+      for(const items in cartItems){
+        let itemInfo=products.find((product)=>product.id== items);
+        for(const item in cartItems[items]){
+          try{
+            if(cartItems[items][item] > 0){
+              totalAmount += (itemInfo.price *cartItems[items][item]);
+              // console.log(totalAmount);              
+            }
+          }catch(err){
+            console.log(err);           
+          }
+        }
+      }
+      return totalAmount.toFixed(2);
     }
 
     const products=[{
@@ -344,7 +370,7 @@ const ShopContextProvider=(props)=>{
     const currency="₹";
     const delivery_fee=40;
     const value={
-        products, currency, delivery_fee,search,setSearch,showSearch,setShowSearch, cartItems,addToCart, getCartCount
+        products, currency, delivery_fee,search,setSearch,showSearch,setShowSearch, cartItems,addToCart, getCartCount, updateQuantity,getCartAmount , navigate
     }
 
     return (
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,9 +1,111 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "../components/Title";
+import CartTotal from "../components/CartTotal";
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const { products, currency, cartItems, updateQuantity, navigate } =
+    useContext(ShopContext);
+  const [cartData, setCartData] = useState([]);
 
-export default Cart
+  useEffect(() => {
+    const tempData = [];
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        if (cartItems[items][item] > 0) {
+          tempData.push({
+            _id: items,
+            size: item,
+            quantity: cartItems[items][item],
+          });
+        }
+      }
+    }
+    setCartData(tempData);
+  }, [cartItems]);
+
+  return (
+    <div className="border-t pt-14">
+      <div className="text-2xl mb-3">
+        <Title text1={"YOUR"} text2={"CART"} />
+      </div>
+      <div>
+        {cartData.map((item, index) => {
+          const productData = products.find(
+            (product) => product.id == item._id,
+          );
+          return (
+            <div
+              key={index}
+              className="py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4"
+            >
+              <div className="flex items-start gap-6">
+                <img
+                  className="w-16 sm:w-20 "
+                  src={productData.images[0]}
+                  alt="cart product image"
+                />
+                <div>
+                  <p className="text-sm sm:text-lg font-medium">
+                    {productData.name}{" "}
+                  </p>
+                  <div className="flex items-center gap-5 mt-2">
+                    <p>
+                      {currency}
+                      {productData.price}
+                    </p>
+                    <p className="px-2 sm:px-3 sm:py-1 border bg-slate-50">
+                      {item.size}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <input
+                onChange={(e) =>
+                  e.target.value == "" || e.target.value == "0"
+                    ? null
+                    : updateQuantity(
+                        item._id,
+                        item.size,
+                        Number(e.target.value),
+                      )
+                }
+                className="border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1"
+                type="number"
+                min={1}
+                defaultValue={item.quantity}
+              />
+              {/* delete icon */}
+              <svg
+                onClick={() => updateQuantity(item._id, item.size, 0)}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6 cursor-pointer mr-4 "
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                />
+              </svg>
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="flex justify-end my-20">
+        <div className="w-full sm:w-112.5">
+          <CartTotal/>
+          <div className="w-full text-end">
+            <button onClick={()=>navigate('/place-order')} className="bg-black text-white text-sm my-8 px-8 py-3 hover:scale-105 cursor-pointer">Proceed to Checkout</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
