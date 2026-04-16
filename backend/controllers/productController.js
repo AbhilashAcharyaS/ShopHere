8c383e7ce146249db294288c115fff85356e5ce1
AbhilashAcharyaS <abhilashacharya599@gmail.com>
2026-04-16 13:33:57 +0530

    Remove a product api

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -65,7 +65,15 @@ const listProducts = async (req, res) => {
     }
 };
 
-const removeProduct = async (req, res) => {};
+const removeProduct = async (req, res) => {
+    try {
+        await productModel.findByIdAndDelete(req.body.id)
+        res.json({success:true, message:"Product Removed!"})
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+};
 
 const singleProduct = async (req, res) => {};
 
