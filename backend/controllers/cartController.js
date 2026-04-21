d0dc634c2caaca7dc9e2ae16a34c07533698634d
AbhilashAcharyaS <abhilashacharya599@gmail.com>
2026-04-21 16:58:17 +0530

    Feat: Get user cart

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -46,6 +46,11 @@ const updateCart=async(req,res)=>{
 
 const getUserCart=async(req,res)=>{
     try {
+        const {userId}= req.body;
+        const userData= await userModel.findById(userId);
+        let cartData= await userData?.cartData;
+
+        res.json({success:true, cartData})
         
     } catch (error) {
         console.log(error);
