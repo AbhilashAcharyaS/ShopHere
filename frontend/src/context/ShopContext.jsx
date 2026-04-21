ae1ac0a7296e95091c2a77b3a2a1c46219f44eee
AbhilashAcharyaS <abhilashacharya599@gmail.com>
2026-04-21 13:15:57 +0530

    Login, Sign up from frontend

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,109 +1,125 @@
 import { createContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
-import {useNavigate} from "react-router-dom"
-import axios from "axios"
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-export const ShopContext=createContext();
+export const ShopContext = createContext();
 
-const ShopContextProvider=(props)=>{
-  const [search,setSearch]=useState("");
-  const [showSearch,setShowSearch]=useState(false);
-  const [cartItems, setCartItems]=useState({});
-  const [products,setProducts]=useState([]);
-  const navigate = useNavigate()
-  const currency="₹";
-  const delivery_fee=40;
-  const backendUrl= import.meta.env.VITE_BACKEND_URL;
+const ShopContextProvider = (props) => {
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
+  const [cartItems, setCartItems] = useState({});
+  const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
+  const currency = "₹";
+  const delivery_fee = 40;
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const [token, setToken] = useState("");
 
-  const getProductsData=async()=>{
-    try {      
-      const res=await axios.get(backendUrl+"/api/product/list");
-      if(res.data.success){
-        setProducts(res.data.products);      
-      }else{
-        toast.error(res.data.message)
+  const getProductsData = async () => {
+    try {
+      const res = await axios.get(backendUrl + "/api/product/list");
+      if (res.data.success) {
+        setProducts(res.data.products);
+      } else {
+        toast.error(res.data.message);
       }
     } catch (error) {
       console.log(error);
-      toast.error(error.message)
+      toast.error(error.message);
     }
-  }
+  };
+
+  const addToCart = async (itemId, size) => {
+    if (!size) {
+      toast.error("Select Product Size!");
+      return;
+    }
+    let cartData = structuredClone(cartItems);
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+
+    setCartItems(cartData);
+  };
+
+  const getCartCount = () => {
+    let totalCount = 0;
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalCount += cartItems[items][item];
+          }
+        } catch (err) {}
+      }
+    }
+    return totalCount;
+  };
+
+  const updateQuantity = async (itemId, size, quantity) => {
+    let cartData = structuredClone(cartItems);
+    cartData[itemId][size] = quantity;
+    setCartItems(cartData);
+  };
+
+  const getCartAmount = () => {
+    let totalAmount = 0;
+    for (const items in cartItems) {
+      let itemInfo = products.find((product) => product._id == items);
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalAmount += itemInfo?.price * cartItems[items][item];
+            // console.log(totalAmount);
+          }
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    }
+    return totalAmount.toFixed(2);
+  };
+
+  useEffect(() => {
+    getProductsData();
+  }, []);
 
   useEffect(()=>{
-    getProductsData()
+    if(!token && localStorage.getItem("token")){
+      localStorage.getItem("token");
+    }
   },[])
 
-    const addToCart=async(itemId,size)=>{
-      if(!size){
-        toast.error("Select Product Size!");
-        return;
-      }
-      let cartData= structuredClone(cartItems);
-      if(cartData[itemId]){
-        if(cartData[itemId][size]){
-          cartData[itemId][size] += 1;
-        }
-        else{
-          cartData[itemId][size] =1;
-        }
-      }
-      else{
-        cartData[itemId]={};
-        cartData[itemId][size] =1;
-      }
+  const value = {
+    products,
+    currency,
+    delivery_fee,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
+    cartItems,
+    addToCart,
+    getCartCount,
+    updateQuantity,
+    getCartAmount,
+    navigate,
+    token,
+    setToken,
+    backendUrl,
+  };
 
-      setCartItems(cartData);
-    }
-
-    const getCartCount=()=>{
-      let totalCount=0;
-      for(const items in cartItems){
-        for(const item in cartItems[items]){
-          try{
-            if(cartItems[items][item] >0){
-              totalCount+=cartItems[items][item]
-            }
-          }catch(err){
-
-          }
-        }
-      }
-      return totalCount;
-    }
-
-    const updateQuantity=async(itemId, size, quantity)=>{
-      let cartData=structuredClone(cartItems);
-      cartData[itemId][size]=quantity;
-      setCartItems(cartData)
-    }
-
-    const getCartAmount=()=>{
-      let totalAmount=0;
-      for(const items in cartItems){
-        let itemInfo=products.find((product)=>product._id== items);
-        for(const item in cartItems[items]){
-          try{
-            if(cartItems[items][item] > 0){
-              totalAmount += (itemInfo?.price *cartItems[items][item]);
-              // console.log(totalAmount);              
-            }
-          }catch(err){
-            console.log(err);           
-          }
-        }
-      }
-      return totalAmount.toFixed(2);
-    }
-
-    const value={
-         products, currency, delivery_fee,search,setSearch,showSearch,setShowSearch, cartItems,addToCart, getCartCount, updateQuantity,getCartAmount , navigate
-    }
-
-    return (
-        <ShopContext.Provider value={value}>
-            {props.children}
-        </ShopContext.Provider>
-    )
-}
+  return (
+    <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
+  );
+};
 
 export default ShopContextProvider;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,31 +1,117 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 const Login = () => {
-  const [currentState, setCurrentState]= useState("Sign up");
+  const [currentState, setCurrentState] = useState("Sign up");
+  const { token, setToken, navigate, backendUrl } = useContext(ShopContext);
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
-  const handleSubmit=async(e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
-  }
+    try {
+      if (currentState === "Sign up") {
+        const res = await axios.post(backendUrl + "/api/user/register", {
+          name,
+          email,
+          password,
+        });
+        if (res.data.success) {
+          setToken(res.data.token);
+          localStorage.setItem("Token", res.data.token);
+          toast.success("Signed Up!");
+        } else {
+          toast.error(res.data.message);
+        }
+      } else {
+        const res = await axios.post(backendUrl + "/api/user/login", {
+          email,
+          password,
+        });
+        if (res.data.success) {
+          setToken(res.data.token);
+          localStorage.setItem("token", res.data.token);
+          toast.success("Logged In");
+        } else {
+          toast.error(res.data.message);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(()=>{
+    if(token){
+      navigate("/")
+    }
+  },[token])
 
   return (
-    <form className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800' onSubmit={handleSubmit}>
+    <form
+      className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800"
+      onSubmit={handleSubmit}
+    >
       <div className="inline-flex items-center gap-2 mb-2 mt-10">
-        <p className='prata-regular text-3xl'>{currentState} </p>
-        <hr className='border-none h-[1.5px] w-8 bg-gray-800 '/>
+        <p className="prata-regular text-3xl">{currentState} </p>
+        <hr className="border-none h-[1.5px] w-8 bg-gray-800 " />
       </div>
-      { currentState === 'Login' ? "" : <input type="text" className='w-full px-3 py-2 border border-gray-800' placeholder='Name' required />}
-      <input type="email" className='w-full px-3 py-2 border border-gray-800' placeholder='Email' required/>
-      <input type="password" className='w-full px-3 py-2 border border-gray-800' placeholder='Password' required/>
+      {currentState === "Login" ? (
+        ""
+      ) : (
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          className="w-full px-3 py-2 border border-gray-800"
+          placeholder="Name"
+          required
+        />
+      )}
+      <input
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        type="email"
+        className="w-full px-3 py-2 border border-gray-800"
+        placeholder="Email"
+        required
+      />
+      <input
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        type="password"
+        className="w-full px-3 py-2 border border-gray-800"
+        placeholder="Password"
+        required
+      />
       <div className="w-full flex justify-between text-sm -mt-2">
         <p className="cursor-pointer">Forgot your password?</p>
-        {currentState === 'Login'? <p className='cursor-pointer hover:border-b' onClick={()=>setCurrentState("Sign up")}>Create Account</p> : <p className='cursor-pointer hover:border-b' onClick={()=>setCurrentState('Login')}>Login Here</p> }
-
+        {currentState === "Login" ? (
+          <p
+            className="cursor-pointer hover:border-b"
+            onClick={() => setCurrentState("Sign up")}
+          >
+            Create Account
+          </p>
+        ) : (
+          <p
+            className="cursor-pointer hover:border-b"
+            onClick={() => setCurrentState("Login")}
+          >
+            Login Here
+          </p>
+        )}
       </div>
 
-      <button className='bg-black text-white font-light px-8 py-2 mt-4 rounded-md hover:scale-105 cursor-pointer transition ease-in-out'>{currentState==='Login'?'Sign In':'Sign Up'} </button>
-
+      <button className="bg-black text-white font-light px-8 py-2 mt-4 rounded-md hover:scale-105 cursor-pointer transition ease-in-out">
+        {currentState === "Login" ? "Sign In" : "Sign Up"}{" "}
+      </button>
     </form>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
